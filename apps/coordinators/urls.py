3f43de7311cf9eed570dec2031384a5155cf5689
PortillaXpert <jrivera082002@gmail.com>
2025-06-03 10:34:26 -0500

    Add coordinator_dashboard..

--- apps/coordinators/urls.py
+++ apps/coordinators/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from apps.coordinators.views.coordinator_views import coordinator_detail, coordinator_update
-
+from apps.coordinators.views.coordinator_dashboard import CoordinatorDashboardView
 app_name = "coordinators"
 
 urlpatterns = [
+    path("", CoordinatorDashboardView.as_view(), name="dashboard"),
     path("", coordinator_detail.as_view(), name="detail"),
     path("editar/", coordinator_update.as_view(), name="edit"),
 ]
