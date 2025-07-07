f12db46d1e284e017a868c6840e1f8e6653ed1d7
PortillaXpert <jrivera082002@gmail.com>
2025-07-07 02:04:41 -0500

    Add custom clean method and capacity/location checks to Environment model.

--- apps/environments/models/environment.py
+++ apps/environments/models/environment.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 class Environment(models.Model):
@@ -14,6 +15,22 @@ class Environment(models.Model):
         default=Env_types.VIRTUAL)
     capacity = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
+
+    def validate_capacity(self):
+        if self.capacity < 1:
+            raise ValidationError('Capacity must be greater than 0')
+        elif self.capacity > 100:
+            raise ValidationError('Capacity must be less than 100')
+
+    def clean(self):
+        super().clean()
+        if self.env_type == self.Env_types.VIRTUAL and self.location.strip().lower() != 'online':
+            raise ValidationError({'location': 'Virtual environments must have location "Online"'})
+        self.validate_capacity()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
